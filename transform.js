71e1312540112b3045ce6f3d24e57c63efee9999
Michael <kllmmc23@gmail.com>
2020-11-09 09:32:18 -0700

    getalljkeys

--- transform.js
+++ transform.js
@@ -7,3 +7,22 @@ function transform(arr) {
 }
 var input = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
 let a = transform(input)
+
+function getAllKeys(obj) {
+    let newArr = []
+    let arr = []
+    
+    newArr.push(Object.keys(obj))
+    for (key in obj) {
+        arr.push(key)
+    }
+
+    return arr
+}
+var obj = {
+    name : 'Sam',
+    age : 25,
+    hasPets : true
+};
+let b = getAllKeys(obj)
+console.log(b)
