91c38d6555dc0d0eac4f291a65f59a8880b26736
Michael <kllmmc23@gmail.com>
2020-11-08 21:51:40 -0700

    binary-search

--- binarySearch.js
+++ binarySearch.js
@@ -20,20 +20,25 @@
 // * If at any point you calculate the index of the midpoint and get a fractional number, just round it down ("floor" it).
 //310
 function binarySearch(arr, n) {
-  let firstHalf = 0
-  let secondHalf = arr.length - 1
+  let left = 0;
+  let right = arr.length - 1;
 
-  while (firstHalf <= secondHalf) {
-    
+  while (left <= right) {
+    let center = Math.floor(right + left) / 2;
+
+    if (n === arr[center]) {
+      return center;
+    }
+    n < arr[center] ? (right = center - 1) : (left = center + 1);
   }
-  let firstHalf = arr.slice(0, arr.length / 2 + 1)
-  firstHalf[firstHalf.length - 1] < n
-  let secHalf = arr.slice(arr.length / 2 + 1, arr.length)
-  let middle = secHalf[Math.floor(secHalf.length / 2)]
-  secHalf[middle] === n 
-  firstHalf = secHalf.slice(0, secHalf.length / 2 )
-  Number(firstHalf) === n
-
- arr[31]
+  return false
 }
-console.log(binarySearch([1, 3, 16, 22, 31, 33, 34], 31))
+//330
+console.log(binarySearch([1, 3, 16, 22, 31, 33, 34], 31));
+// let left = arr.slice(0, arr.length / 2 + 1)
+// left[left.length - 1] < n
+// let secHalf = arr.slice(arr.length / 2 + 1, arr.length)
+// let center = secHalf[Math.floor(secHalf.length / 2)]
+// secHalf[center] === n
+// left = secHalf.slice(0, secHalf.length / 2 )
+// Number(left) === n
